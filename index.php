731cffcd0885a16dee4650ccff12ea7c24714106
Margarita <qazaciok@gmail.com>
2019-04-16 15:03:43 +0300

    komentaras

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-print 'Veikia su Git!';
-?>
+<!DOCTYPE>
+<html>
+    <head>
+    <title>Aš valdau HTML</title>
+    </head>
+    <body>
+        <h1>Margarita-HTML, asė!</h1>
+        <p>Aš moku kurti statinį turinį</p>
+        
+    </body>
+</html>
